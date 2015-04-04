787d877bd200d3fa3eb3bf63e02053804ad39347
Tõnis Pool <tonis.pool@zeroturnaround.com>
2015-04-04 19:28:02 +0300

    Added basic authentication

--- server.js
+++ server.js
@@ -1,19 +1,59 @@
 var WebSocketServer = require("ws").Server;
 var http = require("http");
 var express = require('express');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var flash = require('connect-flash');
 var app = express();
 var port = process.env.PORT || 5000;
-//var passport = require('passport'), LocalStrategy = require('passport-local').Strategy;
+var passport = require('passport'), LocalStrategy = require('passport-local').Strategy;
 
 var _ = require('underscore');
 var fs = require('fs');
 var authenticatedSocket = null;
 
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+      process.nextTick(function () {
+        if (process.env.PASSWORD !== password) return done(null, false, { message: 'Incorrect password.' });
+        return done(null, {name:"külaelanik"});
+      });
+    }
+));
+
+passport.serializeUser(function(user, done) {
+  done(null, user.name);
+});
+
+passport.deserializeUser(function(id, done) {
+  done(null, {name:"külaelanik"});
+});
+
 // Define static HTML files
-app.use(express.static(__dirname + '/html'));
+app.use(express.static(__dirname + '/static'));
 app.use("/data", express.static(__dirname + '/temp'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session({secret: '35gas()"Fe'}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
 
 var API_KEY = process.env.INGLISTE_SHARED_SECRET;
+
+app.post('/login',
+    passport.authenticate('local', { failureRedirect: '/login', failureFlash: true }),
+    function(req, res) {
+      res.redirect('/');
+    });
+
+app.get('/login',function(req, res){
+  res.sendFile(__dirname + '/html/login.html');
+});
+
+app.get('/', ensureAuthenticated,function(req, res){
+  res.sendFile(__dirname + '/html/index.html');
+});
 
 // define GET request for /send/deviceName/buttonName
 app.get('/send/:device/:key', function(req, res) {
@@ -63,3 +103,8 @@ wss.on("connection", function(ws) {
     }
   });
 });
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/login');
+}
